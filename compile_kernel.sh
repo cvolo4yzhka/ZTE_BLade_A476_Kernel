e8b8ef23c5a8f3a667710a3c598f0ebcbb3eecde
cvolo4yzhka <34650243+cvolo4yzhka@users.noreply.github.com>
2018-09-10 13:25:37 +0300

    Update compile_kernel.sh

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -4,5 +4,6 @@ mkdir out
 export ARCH=arm64
 export CROSS_COMPILE=~/android/toolchain/bin/aarch64-linux-android-
 export TARGET_BUILD_VARIANT=user
-make O=out zte_a476_otg_defconfig 
+export  KBUILD_OUTPUT=out
+make zte_a476_otg_defconfig 
 make Image.gz-dtb -j5

a2152390c4326d4aedb7345cf74d9f9526be1520
cvolo4yzhka <cvolo4yzhka@mail.ru>
2018-10-23 09:30:56 +0300

    small correction

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -3,8 +3,9 @@
 mkdir out
 #set architectute
 export ARCH=arm64
+export SUBARCH=arm64
 #set cross compiller path
-export CROSS_COMPILE=~/android/toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/Kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 #set build variant(optinal)
 export TARGET_BUILD_VARIANT=user
 #set directory for build output

e7f88b6ee04aa5b75d7ffd42b31e559985b7ee80
cvolo4yzhka <cvolo4yzhka@mail.ru>
2018-10-23 10:26:20 +0300

    clear

--- compile_kernel.sh
+++ compile_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #create out directory
-mkdir out
+mkdir out64
 #set architectute
 export ARCH=arm64
 export SUBARCH=arm64
@@ -9,9 +9,9 @@ export CROSS_COMPILE=~/Kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-li
 #set build variant(optinal)
 export TARGET_BUILD_VARIANT=user
 #set directory for build output
-export  KBUILD_OUTPUT=out
+export  KBUILD_OUTPUT=out64
 #set defconfig
 make zte_a476_otg_defconfig 
 #start compile
-make Image.gz-dtb -j5 2>&1 | tee out/build.log
+make Image.gz-dtb -j5 2>&1 | tee out64/build.log
 
